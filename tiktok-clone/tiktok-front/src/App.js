47b4d0f1412187deb6ea679abee1ce7d9dc7d627
Nika <cv3910@innovaccion.mx>
2021-12-01 20:33:10 -0600

    axios connected to heroku

--- tiktok-clone/tiktok-front/src/App.js
+++ tiktok-clone/tiktok-front/src/App.js
@@ -1,39 +1,37 @@
+import { useEffect, useState } from 'react'
 import './App.css';
 import Video from './Video';
+import axios from './axios.js'
 
 function App() {
+  
+  const [videos, setVideos] = useState([]);
+  
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await axios.get('/v2/posts');
+      setVideos(response.data);
+
+      return response;
+    }
+
+    fetchPosts();
+  }, []);
+
   return (
     <div className="app">
       <div className="app-videos" >
-      <Video 
-      url="https://v16m.tiktokcdn.com/2c3e82dbf397d082edeee97881129e93/61a2f52d/video/tos/alisg/tos-alisg-pve-0037c001/fff94bd6b5ac460f972a42375d87783b/?a=1233&amp;br=7674&amp;bt=3837&amp;cd=0%7C0%7C1&amp;ch=0&amp;cr=0&amp;cs=0&amp;cv=1&amp;dr=0&amp;ds=3&amp;er=&amp;ft=wZ~R9FRTkag3-I&amp;l=202111272119010102230661710377F05F&amp;lr=tiktok_m&amp;mime_type=video_mp4&amp;net=0&amp;pl=0&amp;qs=0&amp;rc=anI6NDo6ZmQ7ODMzODczNEApOGQ1ZWRpOWVpN2k0ODszOmdxams2cjQwcS1gLS1kMS1zczUxMS4wX2IwMy8tYS5eLi46Yw%3D%3D&amp;vl=&amp;vr="
-      channel="DrossRotzank"
-      description="El video que estas apunto de ver no es un top"
-      song="Burrito sabanero - MR. Asita"
-      likes={123}
-      messages={456}
-      share={789}
-      />
-
-      <Video 
-      url="https://v16m.tiktokcdn.com/2c3e82dbf397d082edeee97881129e93/61a2f52d/video/tos/alisg/tos-alisg-pve-0037c001/fff94bd6b5ac460f972a42375d87783b/?a=1233&amp;br=7674&amp;bt=3837&amp;cd=0%7C0%7C1&amp;ch=0&amp;cr=0&amp;cs=0&amp;cv=1&amp;dr=0&amp;ds=3&amp;er=&amp;ft=wZ~R9FRTkag3-I&amp;l=202111272119010102230661710377F05F&amp;lr=tiktok_m&amp;mime_type=video_mp4&amp;net=0&amp;pl=0&amp;qs=0&amp;rc=anI6NDo6ZmQ7ODMzODczNEApOGQ1ZWRpOWVpN2k0ODszOmdxams2cjQwcS1gLS1kMS1zczUxMS4wX2IwMy8tYS5eLi46Yw%3D%3D&amp;vl=&amp;vr="
-      channel="DrossRotzank"
-      description="El video que estas apunto de ver no es un top"
-      song="Burrito sabanero - MR. Asita"
-      likes={123}
-      messages={456}
-      share={789}
-      />
-
-      <Video 
-      url="https://v16m.tiktokcdn.com/2c3e82dbf397d082edeee97881129e93/61a2f52d/video/tos/alisg/tos-alisg-pve-0037c001/fff94bd6b5ac460f972a42375d87783b/?a=1233&amp;br=7674&amp;bt=3837&amp;cd=0%7C0%7C1&amp;ch=0&amp;cr=0&amp;cs=0&amp;cv=1&amp;dr=0&amp;ds=3&amp;er=&amp;ft=wZ~R9FRTkag3-I&amp;l=202111272119010102230661710377F05F&amp;lr=tiktok_m&amp;mime_type=video_mp4&amp;net=0&amp;pl=0&amp;qs=0&amp;rc=anI6NDo6ZmQ7ODMzODczNEApOGQ1ZWRpOWVpN2k0ODszOmdxams2cjQwcS1gLS1kMS1zczUxMS4wX2IwMy8tYS5eLi46Yw%3D%3D&amp;vl=&amp;vr="
-      channel="DrossRotzank"
-      description="El video que estas apunto de ver no es un top"
-      song="Burrito sabanero - MR. Asita"
-      likes={123}
-      messages={456}
-      share={789}
-      />
+        {videos.map(({url, channel, description, song, likes, messages, shares}) => (
+          <Video 
+            url={url}
+            channel={channel}
+            description={description}
+            song={song}
+            likes={likes}
+            shares={shares}
+            messages={messages}
+          />
+        ))}
       </div>
     </div>
   );
